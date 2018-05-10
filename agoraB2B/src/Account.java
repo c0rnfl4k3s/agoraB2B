4a81d19e478290246dce9797116bede4cab32d58
Andi <wenzelandreas97@mail.de>
2018-05-10 16:50:10 +0200

    123test

--- agoraB2B/src/Account.java
+++ agoraB2B/src/Account.java
@@ -2,18 +2,26 @@ import java.util.*;
 
 public class Account {
 
-    public ArrayList<Benutzer> users;
+    public static int anzahlAccounts;
+    public Benutzer user;
     public String name;
     private String passwort;
     public int userID;
     public int anzahlRechnungen;
     public Postfach postfach;
     public Bankkonto bankkonto; // als Array-List, wenn die möglichkeit für mehrere da sein muss
-    public Bestellung bestellung;
+    public ArrayList<Bestellung> bestellungen;
     public Einkaufswagen cart;
     public ArrayList<Produkt> angeboteneProdukte;
 
-    public Account
+    public Account(Benutzer nUser, String nName, String nPasswort, Bankkonto nBankkonto){
+        anzahlAccounts++;
+        userID = anzahlAccounts;
+        user = nUser;
+        name = nName;
+        passwort = nPasswort;
+        bankkonto = nBankkonto;
+    }
 
     public void logout(){
 
